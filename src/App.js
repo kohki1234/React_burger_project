979895b04123be87222a50b6f23f7628217650f8
Kohki Sato <you@example.com>
2018-06-16 20:16:37 +0900

    This is the first commit to make burger app in React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Hi, I am a react app !! </h1>
+      <p>I am enjoying my life??</p>
+      </div>
+    );
+  }
+}
+
+export default App;
